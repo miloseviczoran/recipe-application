2d2a68ab9146c21d0ef9b1d271ac53a7b1f773e4
miloseviczoran <mag.oec.milosevic.zoran@gmail.com>
2024-12-08 20:24:15 +0100

    new function pozdrav1

--- src/js/controller.js
+++ src/js/controller.js
@@ -140,5 +140,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView._addHandlerUpload(controlAddRecipe);
+  pozdaravFunkcija();
 };
 init();
