13b7fca0b91695f48df116bc1e84991871b1a128
miloseviczoran <mag.oec.milosevic.zoran@gmail.com>
2024-12-08 20:23:49 +0100

    new function pozdrav

--- src/js/controller.js
+++ src/js/controller.js
@@ -128,6 +128,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const pozdaravFunkcija = function () {
+  console.log('POZDRAV MAMA');
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
