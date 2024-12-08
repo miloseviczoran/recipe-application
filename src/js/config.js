b7175828afaf752486475f7f380c74b08dff78b1
miloseviczoran <mag.oec.milosevic.zoran@gmail.com>
2024-12-08 15:14:12 +0100

    Your commit message here

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '0c7c02d9-d52e-44f8-ad00-73354cfb13dc';
+export const MODAL_CLOSE_SEC = 2.5;
---
+++ src/js/views/resultsView.js
@@ -0,0 +1,18 @@
+import View from './View.js';
+import previewView from './previewView.js';
+
+import icons from '../../img/icons.svg'; // Parcel 1
+import icons from 'url:../../img/icons.svg'; // Parcel 2
+
+class ResultsView extends View {
+  _parentEl = document.querySelector('.results');
+  _errorMessage = 'No recipes found for your query! Please try again :)';
+  _message = '';
+
+  _generateMarkup() {
+    // console.log(this._data);
+    return this._data.map(result => previewView.render(result, false)).join('');
+  }
+}
+
+export default new ResultsView();
